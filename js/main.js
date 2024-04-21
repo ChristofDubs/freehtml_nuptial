695f345809e453ce6d105cf314433379254c56c6
Christof Dubs <christof.dubs@gmx.ch>
2024-04-21 16:29:59 +0200

    specify time in UTC

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Aug 16, 2024 16:00:00").getTime();
+	var countDownDate = new Date("Aug 16, 2024 14:00:00 UTC").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
