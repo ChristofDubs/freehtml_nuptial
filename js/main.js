2c1b86e4f764887481689d8e066af46ae9a31918
Christof Dubs <christof.dubs@gmx.ch>
2024-04-23 15:36:34 +0200

    get current time in UTC

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@
 	var x = setInterval(function() {
 
 	// Get todays date and time
-	var now = new Date().getTime();
+	var now = Date.now();
 
 	// Find the distance between now an the count down date
 	var distance = countDownDate - now;
